0825fba10d6c293ead270d3c9c416fa5a4c890bb
Pavel <never1mind@yandex.ru>
2022-10-19 22:45:01 +0300

    Семинар 2 - задание 1

---
+++ семинар 2/Задание 1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число от 100 до 999 включительно =>");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>=100 && number<999)
+{
+    int secondNumber = number%100;
+    secondNumber = secondNumber/10;
+    Console.WriteLine($"Вторая цифра введенного числа => {secondNumber} ");
+}
+else{
+    Console.WriteLine($"Число {number} не входит в диапазонон от 100 до 999");
+}
