1d2aacdcbd7895e5a2e766a07d0664b3c5930955
Pavel <never1mind@yandex.ru>
2022-11-06 18:27:07 +0300

    семинар 3 - сделал ДЗ

---
+++ семинар 3/задание 1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Без использования строк
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 10000 || number > 99999)
+    {
+        Console.WriteLine("Введено не пятизначное число");
+        return false;
+    }
+    return true;
+}
+
+int ValidatePalindrom(int number)
+{
+    int x = number / 1000;
+    int y = (number % 100) % 10 * 10 + (number % 100) / 10;
+    return x - y;
+}
+
+
+void result(int number)
+{
+  if (ValidatePalindrom(number) == 0)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}  
+}
+
+
+int number = Prompt("Введите пятизначное число => ");
+
+if(ValidateNumber(number))
+{
+    result(ValidatePalindrom(number));
+}
---
+++ семинар 3/задание 2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int x1 = Prompt("Введите координату х точки А: ");
+int y1 = Prompt("Введите координату y точки А: ");
+int z1 = Prompt("Введите координату z точки А: ");
+int x2 = Prompt("Введите координату x точки B: ");
+int y2 = Prompt("Введите координату y точки B: ");
+int z2 = Prompt("Введите координату z точки B: ");
+
+int x = x2 - x1;
+int y = y2 - y1;
+int z = z2 - z1;
+
+double Lenght = Math.Sqrt(x * x + y * y + z * z);
+
+Console.WriteLine($"Расстояние между точками А и В равно {Lenght:f2}");
